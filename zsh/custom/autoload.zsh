e058649d474e2b4d93af9310b7744fea27ee5592
thomashuang <lyanghwy@gmail.com>
2015-02-07 11:44:28 +0800

    Initialize

---
+++ zsh/custom/autoload.zsh
@@ -0,0 +1,7 @@
+# Add yourself some shortcuts to projects you often work on
+# Example:
+#
+# brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
+#
+
+source /usr/local/bin/virtualenvwrapper.sh
