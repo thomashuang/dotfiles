a76818fee8943fa951b306faf24dc309396b0488
thomashuang <lyanghwy@gmail.com>
2015-03-13 18:02:50 +0800

    updated zsh

--- zsh/custom/autoload.zsh
+++ zsh/custom/autoload.zsh
@@ -3,5 +3,5 @@
 #
 # brainstormr=/Users/robbyrussell/Projects/development/planetargon/brainstormr
 #
-
+export WORKON_HOME=$HOME/.virtualenvs       # <-- Where all
 source /usr/local/bin/virtualenvwrapper.sh
