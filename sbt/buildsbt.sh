c14f0483db43ed8f93d79e329d06cdb24bd38395
thomashuang <lyanghwy@gmail.com>
2015-04-01 16:42:41 +0800

    Update build sh

--- sbt/buildsbt.sh
+++ sbt/buildsbt.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+echo "mkdir $1 and enter"
+mkdir $1
+cd "$1"
 mkdir -p src/{main,test}/{java,resources,scala}
 mkdir lib project target
 # create an initial build.sbt file
-echo 'name := "MyProject"
-version := "1.0"
-scalaVersion := "2.10.0"' > build.sbt
+
+result=$(printf "name := \"%s\"\nversion := \"1.0\"\nscalaVersion := \"2.11.6\"" "$1")
+echo -e "$result" > build.sbt
+echo "success"
